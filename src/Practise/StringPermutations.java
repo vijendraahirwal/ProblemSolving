74df1a5289946f41a0efed78f9eda728bfbcb5cb
vijendraahirwal <vijendraahirwal@gmail.com>
2023-02-19 21:33:04 -0500

    StringPermutationUsingRecursion

---
+++ src/Practise/StringPermutations.java
@@ -0,0 +1,20 @@
+package Practise;
+import java.util.ArrayList;
+
+public class StringPermutations {
+    
+    public static void main(String[] args) {
+        String s = "ABC";
+        permutation(s);
+    }
+    
+    public static ArrayList<String> permutation(String S)
+    {
+        //Your code here
+        ArrayList<String> arrlist= new ArrayList<String>();
+        if(S.length() == 2){
+            
+        }
+        return arrlist;
+    }
+}
