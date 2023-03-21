97aa8a7f592c21fdc50eb13db43e430535ba8b8a
vijendraahirwal <vijendraahirwal@gmail.com>
2023-03-21 18:28:23 -0400

    LinkedList

---
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,14 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        
+    }
+
+    class Node{
+        int value;
+        Node rightChild;
+        Node leftChild;
+        Node()
+    }
+}
